32923d8462f83e82cd3f38083227c355c0ae58f3
Volatil <contacto@volatil.cl>
2018-09-13 18:29:27 -0300

    Actualizacion

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit59441168b75001242136fde52183bc77::getLoader();
+return ComposerAutoloaderInitcd18da2edb26aadba05f70470b7b3a92::getLoader();
---
+++ wp-content/wflogs/config-synced.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:19:{s:6:"apiKey";s:160:"9abf07f2c4d9ae78f63cfdf70697efccc99507ca337869b70c8ff5dea134d24769e13b4812ce1ef0d24885e402974e21035312a0b58d899cc20fac1a3fdadef4329ee84c8f9b6ea7a61b24f5be26adf2";s:6:"isPaid";b:0;s:7:"siteURL";s:28:"http://localhost/clickcondon";s:7:"homeURL";s:28:"http://localhost/clickcondon";s:14:"whitelistedIPs";s:0:"";s:9:"howGetIPs";s:0:"";s:25:"howGetIPs_trusted_proxies";s:0:"";s:13:"pluginABSPATH";s:28:"C:\xampp\htdocs\clickcondon/";s:11:"other_WFNet";b:1;s:9:"serverIPs";s:24:"["201.214.77.229","::1"]";s:15:"blockCustomText";s:0:"";s:13:"timeoffset_wf";s:3:"142";s:23:"advancedBlockingEnabled";N;s:21:"betaThreatDefenseFeed";b:0;s:20:"disableWAFIPBlocking";s:1:"0";s:13:"patternBlocks";N;s:13:"countryBlocks";N;s:11:"otherBlocks";N;s:8:"lockouts";N;}
